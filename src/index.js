6bd5360eceae9504228550a9af304ad6a121bf41
outdoorcoder <talarsen74@gmail.com>
2021-05-15 20:26:41 -0700

    reactMapFilterReduceMethods

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
-var numbers = [3, 56, 2, 48, 5];
+import emojipedia from "./emojipedia.js";
 
-//Map -Create a new array by doing something with each item in an array.
+const trucString = emojipedia.map(function (string) {
+  return string.meaning.substring(0, 100);
+});
 
-//Filter - Create a new array by keeping the items that return true.
+console.log(trucString);
 
-//Reduce - Accumulate a value by doing something to each item in an array.
+// var numbers = [3, 56, 2, 48, 5];
 
-//Find - find the first item that matches from an array.
+// //Map loop through an array and does something with each item  -Create a new array by doing something with each item in an array.
 
-//FindIndex - find the index of the first item that matches.
+// // function doubleItems(x) {
+// //   return x * 2;
+// // }
+// // const newNumbers = numbers.map(doubleItems)
+
+// // console.log(newNumbers);
+
+// //Filter - Create a new array by keeping the items that return true.
+
+// // const newNumbers = numbers.filter( function (num) {
+// //   return num > 10
+// // })
+
+// // console.log(newNumbers);
+
+// //with forEach
+// var newNumbers = [];
+
+// numbers.forEach(function(num) {
+//   if(num < 10) {
+//     newNumbers.push(num)
+//   }
+// })
+
+// console.log(newNumbers)
+// //Reduce - Accumulate a value by doing something to each item in an array.
+
+// //Find - find the first item that matches from an array.
+
+// var numbers = [3, 56, 2, 48, 5];
+
+// //FindIndex - find the index of the first item that matches.
